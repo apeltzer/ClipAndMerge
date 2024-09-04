6028e78c82c3696752dd5ed088c4d1a7f233e2c5
Alexander Peltzer <apeltzer@users.noreply.github.com>
2024-09-04 15:33:19 +0200

    Update MergeScript.java

--- src/main/java/main/MergeScript.java
+++ src/main/java/main/MergeScript.java
@@ -53,7 +53,7 @@ import threads.ReverseClipperThread;
  */
 public class MergeScript {
 	
-	public static final String VERSION = "1.7.8";
+	public static final String VERSION = "1.7.9";
 	public static final String TITLE = "ClipAndMerge (v. " + VERSION +")";
 	
 	public long startTime;
